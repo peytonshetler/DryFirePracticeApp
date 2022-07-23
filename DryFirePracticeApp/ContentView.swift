9b61eaff939f34caa107357695a7ed1508ac8d62
Peyton Shetler <peyton.shetler@gmail.com>
2022-07-23 14:27:58 -0500

    Initial Commit

---
+++ DryFirePracticeApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  DryFirePracticeApp
+//
+//  Created by Peyton Shetler on 7/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DryFirePracticeApp/DryFirePracticeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DryFirePracticeAppApp.swift
+//  DryFirePracticeApp
+//
+//  Created by Peyton Shetler on 7/23/22.
+//
+
+import SwiftUI
+
+@main
+struct DryFirePracticeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
